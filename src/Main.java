dba1486e55672eb9e62a0809bd00d2f16b97d552
SSANG <ssang@labsd.net>
2024-07-08 17:36:12 +0900

    :sparkles: Add the userDAO class
    - USER의 DAO 클래스를 만듦
    - 로컬 DB(mysql)과 통신 성공

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("blackship");
+        user.setName("백기룡");
+        user.setPassword("loved");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+
     }
 }
