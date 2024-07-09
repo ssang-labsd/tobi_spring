054478538f5303c5850dce0f78a182ea721689e2
SSANG <ssang@labsd.net>
2024-07-09 15:50:24 +0900

    :recycle: 팩토리 이용한 userDaoTest 기능 분리

--- src/UserDaoTest.java
+++ src/UserDaoTest.java
@@ -1,5 +1,6 @@
 import springbook.user.dao.ConnectionMaker;
 import springbook.user.dao.DConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -7,12 +8,10 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
-        user.setId("blueship");
+        user.setId("purleship");
         user.setName("흑기룡");
         user.setPassword("done");
 
